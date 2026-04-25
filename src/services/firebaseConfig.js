15d8f897ba978b254aa3f372d0cba138cc62eb95
dcdigital <dsdiegocoelho@gmail.com>
2026-04-25 14:45:58 -0300

    Update firebaseConfig.js

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -4,18 +4,16 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyArWdCsMvkawY43cnSCR4pw_u9YIUropvw',
+  authDomain: 'fitness-app-7aa96.firebaseapp.com',
+  projectId: 'fitness-app-7aa96',
+  storageBucket: 'fitness-app-7aa96.firebasestorage.app',
+  messagingSenderId: '123102660519',
+  appId: '1:123102660519:web:b71e04b17fea141e629882',
 }
 
 const app = initializeApp(firebaseConfig)
-
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const storage = getStorage(app)
-
 export default app
