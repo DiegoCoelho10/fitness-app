8c583e83da9ebc6b40b4f6bf0b01b3ed10e1cccc
dcdigital <dsdiegocoelho@gmail.com>
2026-04-25 22:09:28 -0300

    Update firebaseConfig.js

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -9,11 +9,10 @@ const firebaseConfig = {
   projectId: 'fitness-app-7aa96',
   storageBucket: 'fitness-app-7aa96.firebasestorage.app',
   messagingSenderId: '123102660519',
-  appId: '1:123102660519:web:b71e04b17fea141e629882',
+  appId: '1:123102660519:web:b71e04b17fea141e629882'
 }
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
 export const storage = getStorage(app)
-export default app
